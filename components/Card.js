222eedd206ec65b767f497319da3601390b760f1
gillsukhman209 <gillsukhman209@gmail.com>
2023-03-21 19:02:28 -0700

    is this wokring

--- components/Card.js
+++ components/Card.js
@@ -3,7 +3,7 @@ import React from "react";
 function Card({ title }) {
   return (
     <div className="w-[400px] h-[500px] bg-[#15223E] border  border-green-300 shadow-2xl rounded-2xl p-4 text-2xl flex justify-center">
-      {titleasdfwhichs}
+      {is it updated now}
     </div>
   );
 }
